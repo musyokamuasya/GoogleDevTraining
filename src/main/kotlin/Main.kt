5922f938c4381ed2dda5ea16bfff3eabfa8c2f6e
musyokamuasya <josephmuasya8@gmail.com>
2022-02-28 16:10:28 +0300

    Function with strings. The function takes in two strings, first name and second name

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,12 +5,13 @@ fun main() {
     * The main program where all the functions are called. This is the main entry into the code
     * */
     println("This program prints sum of 6 and 8")
-    println(sum(6, 8))
+    println(myName("Musyoka", "Muasya"));
 }
-/* Introduction to functions.
-This is an example of basic function which takes in two numbers
-The two numbers are of type int, and the function returns the sum of the two numbers which is an integer
-*/
-fun sum (firstNumber:Int, secondNumber:Int):Int {
-    return firstNumber + secondNumber
+/*
+* This is the second way of returning a function.
+* Instead of creating a return block, you can handle the summation directly after the return time
+* This is an example of expressive Kotlin programming
+* */
+fun myName (firstName: String, secondName:String):String {
+    return "My name is $firstName $secondName"
 }
