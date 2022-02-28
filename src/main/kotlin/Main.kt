6e02958da92c5a4ad92edb450c781285dfe1ddc0
musyokamuasya <josephmuasya8@gmail.com>
2022-02-28 16:41:25 +0300

    String with default values

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,11 +5,11 @@ fun main() {
     * The main program where all the functions are called. This is the main entry into the code
     * */
     println("This program prints sum of 6 and 8")
-    println(myName("Musyoka", "Muasya"));
+    println(myName("Hey", "Musyoka", "Muasya"));
 }
 /*
 * A function takes in two strings, first name and second name
 * */
-fun myName (firstName: String, secondName:String):String {
-    return "My name is $firstName $secondName"
+fun myName (salutations: String = "Mr", firstName: String, secondName:String):String {
+    return "My name is $salutations $firstName $secondName"
 }
