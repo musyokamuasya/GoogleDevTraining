4e2fb3b2467391b297ad160b5cfa44c7543f47a1
musyokamuasya <josephmuasya8@gmail.com>
2022-02-28 16:01:29 +0300

    Introduction to functions, with the main function and function to print a sum of two numbers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,16 @@
-fun main(args: Array<String>) {
-    val instituteName:String = "Msamweni TVET"
-//    The program prints a custom message to the console
 
-    println("Hello, I am from $instituteName")
 
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    /*
+    * The main program where all the functions are called. This is the main entry into the code
+    * */
+    println("This program prints sum of 6 and 8")
+    println(sum(6, 8))
+}
+/* Introduction to functions.
+This is an example of basic function which takes in two numbers
+The two numbers are of type int, and the function returns the sum of the two numbers which is an integer
+*/
+fun sum (firstNumber:Int, secondNumber:Int):Int {
+    return firstNumber + secondNumber
 }
