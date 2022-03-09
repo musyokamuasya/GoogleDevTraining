4d2094cd19955f38ab1a2a6bdd16b933d22a7ff4
musyokamuasya <josephmuasya8@gmail.com>
2022-03-09 11:06:00 +0300

    Usage of arrays and loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,17 @@
 
-fun main() {
-    val rectangle = Rectangle(5, 12.0)
-    println("The area is ${rectangle.area}")
-}
-
 class Rectangle(var height: Int, var length: Double){
     var area = height * length
 }
+fun main() {
+    val rectangle = Rectangle(5, 12.0)
+    println("The area is ${rectangle.area}")
+
+    val learners = listOf("Bakari", "Rukia", "Musyoka", "Mohamed", "Mwanamtiti")
+
+    for(name in learners.indices) {
+        println("Learner at index $name is ${learners[name]}")
+    }
+}
+
+
+
